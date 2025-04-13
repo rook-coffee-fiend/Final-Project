43dc12a0d2042f190b528c6bf64f1ac11ec4fcd7
rook-coffee-fiend <bethdoesgradschool@gmail.com>
2025-04-12 20:05:55 -0400

    Got the pictures to show up on review cards!

--- src/lib/reviews.js
+++ src/lib/reviews.js
@@ -1,18 +1,17 @@
 // credit to https://svelte.dev/playground/dbdad7821bff445ab806fa608a6aac9f?version=5.25.6
 
-//import Alix from './assets/Alix.jpg';
-//import Buddy from './assets/Buddy.jpg';
-//import Herbie from './assets/Herbie.jpg';
-//import JasperI from './assets/Jasper & Pooky.jpg';
-//import JasperII from './assets/Jasper II.jpg';
-//import Moose from './assets/Moose.jpg';
-//import Nanette from './assets/Nanette.jpg';
-//import Nugget from './assets/Nugget.jpg';
-//import Peta from './assets/Peta.jpg';
-//import Pounce from './assets/Pounce V1.jpg';
-//import Stevie from './assets/Stevie.jpg';
-//import Toula from './assets/Toula.jpg';
-//import Wally from './assets/Wally.jpg';
+import Alix from '$assets/Alix.jpg';
+import Buddy from '$assets/Buddy.jpg';
+import Herbie from '$assets/Herbie.jpg';
+import JasperI from '$assets/Jasper & Pooky.jpg';
+import JasperII from '$assets/Jasper II.jpg';
+import Moose from '$assets/Moose.jpg';
+import Nanette from '$assets/Nanette.jpg';
+import Nugget from '$assets/Nugget.jpg';
+import Peta from '$assets/Peta.jpg';
+import Stevie from '$assets/Stevie.jpg';
+import Toula from '$assets/Toula.jpg';
+import Wally from '$assets/Wally.jpg';
 
 
 export const reviews = [
@@ -20,84 +19,84 @@ export const reviews = [
         id: 0,
           name: "Emily & Julian",
           descr: "Buddy, Buffy, & Felix's parents",
-          img: "", 
+          img: Buddy, 
           review: "Buddy is the most handsomest man in the whole world. Felix is a food goblin who will eat through plastic to get at anything remotely edible, and Buffy has to be kept away from Buddy or she'll try to fight him."
       },
     {
         id: 1,
           name: "Lindsay & Maggie",
           descr: "Stevie's moms",
-          img: ""	, 
+          img: Stevie, 
           review: "Stevie is mad"
       },
     {
         id: 2,
           name: "Liz",
           descr: "Peta's mom",
-          img: ""	, 
+          img: Peta, 
           review: "Peta is silly"
       },
     {
         id: 3,
           name: "Louise",
           descr: "Nanette's mom",
-          img: ""	, 
+          img: Nanette, 
           review: "Nanette is creaky"
       },
       {
         id: 4,
           name: "Natalie",
           descr: "Jasper and Pooky's mom",
-          img: ""	, 
+          img: JasperI, 
           review: "These boys are goofy"
       },
       {
         id: 5,
           name: "Morgan",
           descr: "Daphne, Gepetto, and Nugget's mom",
-          img: ""	, 
+          img: Nugget, 
           review: "These cats have a lot of special needs"
       },
       {
         id: 6,
           name: "Samantha",
           descr: "Toula's mom",
-          img: ""	, 
+          img: Toula, 
           review: "Toula is full of energy"
       },
       {
         id: 7,
           name: "Carly",
           descr: "Wally's mom",
-          img: ""	, 
+          img: Wally, 
           review: "Wally's government name is Baby Waluigi"
       },
       {
         id: 8,
           name: "Julia & Val",
           descr: "Alix's humans",
-          img: ""	, 
+          img: Alix, 
           review: "Alix is a snuggly guy"
       },
       {
         id: 9,
           name: "Eric",
           descr: "Moose's mom",
-          img: ""	, 
+          img: Moose, 
           review: "Moose is a big sweetie"
       },
       {
         id: 10,
           name: "Maya",
           descr: "Jasper's mom",
-          img: ""	, 
+          img: JasperII, 
           review: "Jasper is a mischief man and a connessieur of shoelaces"
       },
       {
         id: 11,
           name: "Leslie & Sarah",
           descr: "Herbie's moms",
-          img: ""	, 
+          img: Herbie, 
           review: "Herbie craves snacks of the human variety, but is picky about his own cat food"
       },
     
