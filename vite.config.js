41eff457bd7d5f74cd1dd9ff18238e4e64fff126
Bethany Ward <bethdoesgradschool@gmail.com>
2025-04-24 16:55:29 -0400

    Troubleshoot go live in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
+//import path from 'path';
 
 export default defineConfig({
 	base: '/Final-Project/',
